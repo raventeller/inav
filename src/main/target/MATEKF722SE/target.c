d707f39f3d16c40405227e44962bcec5d2a7b162
raventeller <desparanto@gmail.com>
2026-07-13 14:44:41 -0400

    Update target.c 1

--- src/main/target/MATEKF722SE/target.c
+++ src/main/target/MATEKF722SE/target.c
@@ -15,10 +15,10 @@
  * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* * This file is part of INAV. * * INAV is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. * * INAV is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License * along with INAV. If not, see <http://gnu.org>. */
+
 #include <stdint.h>
-
 #include "platform.h"
-
 #include "drivers/bus.h"
 #include "drivers/io.h"
 #include "drivers/pwm_mapping.h"
@@ -26,26 +26,27 @@
 #include "drivers/pinio.h"
 #include "drivers/sensor.h"
 
-BUSDEV_REGISTER_SPI_TAG(busdev_mpu6000,     DEVHW_MPU6000,      MPU6000_SPI_BUS,    MPU6000_CS_PIN,     NONE,       0,  DEVFLAGS_NONE,  IMU_MPU6000_ALIGN);
-BUSDEV_REGISTER_SPI_TAG(busdev_mpu6500,     DEVHW_MPU6500,      MPU6500_SPI_BUS,    MPU6500_CS_PIN,     NONE,       1,  DEVFLAGS_NONE,  IMU_MPU6500_ALIGN);
+// Register the primary Gyro (MPU6000) using PA15 as CS
+BUSDEV_REGISTER_SPI_TAG(busdev_mpu6000, DEVHW_MPU6000, BUS_SPI1, pinA15, NONE, 0, DEVFLAGS_NONE, CW180_DEG_FLIP);
+
+// Register the secondary fallback Gyro (MPU6500) using PD02 as CS
+BUSDEV_REGISTER_SPI_TAG(busdev_mpu6500, DEVHW_MPU6500, BUS_SPI1, pinD02, NONE, 1, DEVFLAGS_NONE, CW90_DEG);
 
 timerHardware_t timerHardware[] = {
-    DEF_TIM(TIM3, CH1, PB4,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S1   UP1-2   D(1, 4, 5)
-    DEF_TIM(TIM3, CH2, PB5,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S2   UP1-2   D(1, 5, 5)
-    DEF_TIM(TIM3, CH3, PB0,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S3   UP1-2   D(1, 7, 5)
-    DEF_TIM(TIM3, CH4, PB1,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S4   UP1-2   D(1, 2, 5)
-
-    DEF_TIM(TIM2, CH1, PA15,  TIM_USE_OUTPUT_AUTO, 0, 0),   // S5   UP1-7   D(1, 5, 3) - clash with S2
-    DEF_TIM(TIM2, CH2, PB3,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S6   UP1-7   D(1, 6, 3)
-
-    DEF_TIM(TIM4, CH1, PB6,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S7   UP1-6   D(1, 0, 2) // used to be fw motor
-    DEF_TIM(TIM4, CH2, PB7,   TIM_USE_OUTPUT_AUTO, 0, 0),   // S8   UP1-6   D(1, 3, 2) // used to me fw motor
-
-    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_LED, 0, 2),            // LED          D(2, 6, 0)
-
-    // DEF_TIM(TIM9, CH2, PA3,  TIM_USE_PPM, 0, 0),            // PPM, RX2
-
-    DEF_TIM(TIM5, CH3, PA2,  TIM_USE_ANY, 0, 0),            // TX2 & softserial1
+    DEF_TIM(TIM3, CH1, PB4, TIM_USE_OUTPUT_AUTO, 0, 0), // S1 Motor 1 OUT
+    DEF_TIM(TIM3, CH2, PB5, TIM_USE_OUTPUT_AUTO, 0, 0), // S2 Motor 2 OUT
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_OUTPUT_AUTO, 0, 0), // S3 Motor 3 OUT
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_OUTPUT_AUTO, 0, 0), // S4 Motor 4 OUT
+    
+    // REMOVED PA15 TIMER DEF_TIM(TIM2, CH1, PA15...) TO FREE CS PIN FOR GYRO
+    
+    DEF_TIM(TIM2, CH2, PB3, TIM_USE_OUTPUT_AUTO, 0, 0), // S6 OUT
+    DEF_TIM(TIM4, CH1, PB6, TIM_USE_OUTPUT_AUTO, 0, 0), // S7 OUT (Motor 5)
+    DEF_TIM(TIM4, CH2, PB7, TIM_USE_OUTPUT_AUTO, 0, 0), // S8 OUT (Motor 6)
+    
+    DEF_TIM(TIM1, CH1, PA8, TIM_USE_LED, 0, 2),         // LED Strip
+    DEF_TIM(TIM5, CH3, PA2, TIM_USE_ANY, 0, 0),         // TX2 & Softserial1
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
+
