15939e9f48bda24545686cdaf95fd66336620208
raventeller <desparanto@gmail.com>
2026-07-13 15:17:38 -0400

    Update target.h 5

--- src/main/target/MATEKF722SE/target.h
+++ src/main/target/MATEKF722SE/target.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /* * This file is part of INAV. * * INAV is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. * * INAV is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License * along with INAV. If not, see <http://gnu.org>. */
 
 #pragma once
@@ -32,7 +33,7 @@
 #define BEEPER_INVERTED
 
 // *************** SPI1 Gyro & ACC *******************
-// Keep descriptors active so target.c doesn't clash with the linker
+// Keep descriptors active so target.c links cleanly
 #define USE_TARGET_IMU_HARDWARE_DESCRIPTORS
 
 #define USE_SPI
@@ -41,32 +42,30 @@
 #define SPI1_MISO_PIN PA6
 #define SPI1_MOSI_PIN PA7
 
-#define USE_DUAL_GYRO
-
-// Force pin mappings directly over any automated defaults
 #define USE_IMU_MPU6000
 #define IMU_MPU6000_ALIGN CW180_DEG_FLIP
-#undef MPU6000_CS_PIN
-#define MPU6000_CS_PIN PA15
-#define MPU6000_SPI_BUS BUS_SPI1
-
 #define USE_IMU_MPU6500
 #define IMU_MPU6500_ALIGN CW90_DEG
-#undef MPU6500_CS_PIN
-#define MPU6500_CS_PIN PD2
-#define MPU6500_SPI_BUS BUS_SPI1
+
+// Clear default high-speed SPI registers to prevent line capacitance lockups
+#undef SPI1_CLOCK_STANDARD
+#undef SPI1_CLOCK_FAST
+#define SPI1_CLOCK_STANDARD   SpiClock_21MHz
+#define SPI1_CLOCK_FAST       SpiClock_21MHz
 
 // *************** I2C /Baro/Mag *********************
 #define USE_I2C
 
-// Reconfigure Physical I2C Bus 1 (Baro)
+// Force basic software I2C processing to drop the 50 hardware errors
+#define I2C1_OVERCLOCK false
+#define I2C2_OVERCLOCK false
+
 #define USE_I2C_DEVICE_1
 #undef I2C1_SCL
 #undef I2C1_SDA
 #define I2C1_SCL PB8
 #define I2C1_SDA PB9
 
-// Reconfigure Physical I2C Bus 2 (External Compass)
 #define USE_I2C_DEVICE_2
 #undef I2C2_SCL
 #undef I2C2_SDA
@@ -77,11 +76,9 @@
 #define BARO_I2C_BUS BUS_I2C1
 #define USE_BARO_BMP280
 #define USE_BARO_MS5611
-#define USE_BARO_DPS310
-#define USE_BARO_SPL06
 
 #define USE_MAG
-#define MAG_I2C_BUS BUS_I2C2  // Routes external compass to I2C2 pins (B10/B11)
+#define MAG_I2C_BUS BUS_I2C2  
 #define USE_MAG_ALL
 
 #define TEMPERATURE_I2C_BUS BUS_I2C1
@@ -112,7 +109,6 @@
 # define M25P16_CS_PIN PD2
 # define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 #else 
-// Replaced SDCard macro with Flash chip configuration
 # define USE_FLASHFS
 # define USE_FLASH_M25P16
 # define M25P16_SPI_BUS BUS_SPI3
@@ -166,8 +162,8 @@
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
-#define PINIO1_PIN PC8 // VTX power switcher
-#define PINIO2_PIN PC9 // 2xCamera switcher
+#define PINIO1_PIN PC8 
+#define PINIO2_PIN PC9 
 
 // *************** LEDSTRIP ************************
 #define USE_LED_STRIP
